aa65fe71a17d1c0ca7871d1186594a88a2f0ed4a
Pearl Akpan <pearlakpan115@gmail.com>
2022-10-31 16:14:49 +0100

    chore: get user data for profile component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { Routes, Route, NavLink } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import stack from "../assets/Stack.svg";
 import shield from "../assets/Shield.svg";
@@ -13,66 +14,86 @@ import RepositoriesPage from "../views/Repositories/RepositoriesPage";
 import SecurityPage from "../views/Security/SecurityPage";
 
 import "./App.scss";
+import UtilityBar from "./UtilityBar";
 
+export default function App() {
+  const [user, setUser] = useState("");
 
-function App() {
+  useEffect(() => {
+    const fetchUser = () => {
+      fetch("https://api.github.com/users/pearlsky")
+      .then((response) => response.json())
+      .then((data) => setUser(data) )
+      .catch(() => {
+        throw new Error();
+      })
+    }
+
+    fetchUser();
+  }, []);
+
+  const userAvatar = user ? (
+    <img alt="user-avatar" src={user.avatar_url} className="user-img__real"></img>
+  ) : (
+    <div className="user-img__fake"><span>AU</span></div>
+  );
   return (
     <div className="app">
       <header className="header">
         <div className="logo">
-          <img alt="" className="logo-img"/>
+          <img alt="" className="logo-img" />
         </div>
-        <nav>
+        <nav className="nav">
           <ul className="nav__list">
             <li className="nav__item">
-              <NavLink to="/" className="nav__link">
-                <img alt="" className="nav__link__icon" src={stack}/>
+              <NavLink to="/repos" className="nav__link">
+                <img alt="" className="nav__link__icon" src={stack} />
                 <span className="nav__link__text">Repositories</span>
               </NavLink>
             </li>
             <li className="nav__item">
-            <NavLink to="/activity" className="nav__link">
-                <img alt="" className="nav__link__icon" src={lightning}/>
+              <NavLink to="/activity" className="nav__link">
+                <img alt="" className="nav__link__icon" src={lightning} />
                 <span className="nav__link__text">Activity</span>
               </NavLink>
             </li>
             <li className="nav__item">
-            <NavLink to="/people" className="nav__link">
-                <img alt="" className="nav__link__icon" src={people}/>
+              <NavLink to="/people" className="nav__link">
+                <img alt="" className="nav__link__icon" src={people} />
                 <span className="nav__link__text">People</span>
               </NavLink>
             </li>
             <li className="nav__item">
-            <NavLink to="preferences" className="nav__link">
-                <img alt="" className="nav__link__icon" src={pref}/>
+              <NavLink to="preferences" className="nav__link">
+                <img alt="" className="nav__link__icon" src={pref} />
                 <span className="nav__link__text">Preferences</span>
               </NavLink>
             </li>
             <li className="nav__item">
-            <NavLink to="/security" className="nav__link">
-                <img alt="" className="nav__link__icon" src={shield}/>
+              <NavLink to="/security" className="nav__link">
+                <img alt="" className="nav__link__icon" src={shield} />
                 <span className="nav__link__text">Security</span>
               </NavLink>
             </li>
           </ul>
         </nav>
         <div className="user">
-          <div className="user-img__wrap">
-            <img alt="" src="#" className="user-img"></img>
-          </div>
+          <div className="user-img__wrap">{userAvatar}</div>
+          <p className="user-name">{user.name}</p>
+          <p className="user-location">{user.location}</p>
         </div>
       </header>
-      <main className="main"></main>
+      <main className="main">
+        <UtilityBar />
 
-      <Routes>
-        <Route path="/" element={RepositoriesPage}/>
-        <Route path="/activity" element={ActivityPage}/>
-        <Route path="/people" element={PeoplePage}/>
-        <Route path="/preferences" element={PreferencesPage}/>
-        <Route path="/security" element={SecurityPage}/>
-      </Routes>
+        <Routes>
+          <Route path="/repos" element={<RepositoriesPage />} />
+          <Route strict path="/activity" element={<ActivityPage />} />
+          <Route strict path="/people" element={<PeoplePage />} />
+          <Route strict path="/preferences" element={<PreferencesPage />} />
+          <Route strict path="/security" element={<SecurityPage />} />
+        </Routes>
+      </main>
     </div>
   );
 }
-
-export default App;
