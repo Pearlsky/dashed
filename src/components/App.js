d245c8e89af6e77eb25b31901500fdcf24d430ff
pearlsky <pearlakpan115@gmail.com>
2023-01-09 22:01:26 +0100

    refactor: remove useEffect, add Routes for home page

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import { Routes, Route, NavLink} from "react-router-dom";
-import { useState, useEffect } from "react";
+import { Routes, Route, NavLink } from "react-router-dom";
+import { useState } from "react";
 
 import stack from "../assets/Stack.svg";
 import shield from "../assets/Shield.svg";
@@ -17,23 +17,11 @@ import "./App.scss";
 import UtilityBar from "./UtilityBar";
 import IndividualRepoPage from "../views/Repositories/IndividualRepoPage";
 import ErrorBoundary from "./ErrorBoundary";
+import HomePage from "../views/Home/HomePage";
+// import ErrorPage from "../views/ErrorPage/ErrorPage";
 
 export default function App() {
   const [user, setUser] = useState("");
-  const [owner] = useState("pearlsky");
-
-  useEffect(() => {
-    const fetchUser = () => {
-      fetch(`https://api.github.com/users/${owner}`)
-        .then((response) => response.json())
-        .then((data) => setUser(data))
-        .catch(() => {
-          throw new Error();
-        });
-    };
-
-    fetchUser();
-  }, [owner]);
 
   const userAvatar = user ? (
     <img
@@ -43,78 +31,83 @@ export default function App() {
     ></img>
   ) : (
     <div className="user-img__fake">
-      <span>AU</span>
+      <span>NA</span>
     </div>
   );
   return (
     <div className="app">
-      <header className="header">
-        <div className="logo">
-          <img alt="" className="logo-img" />
-        </div>
-        <nav className="nav">
-          <ul className="nav__list">
-            <li className="nav__item">
-              <NavLink to="/" className="nav__link" end>
-                <img alt="" className="nav__link__icon" src={stack} />
-                <span className="nav__link__text">Repositories</span>
-              </NavLink>
-            </li>
-            <li className="nav__item">
-              <NavLink to="/activity" className="nav__link">
-                <img alt="" className="nav__link__icon" src={lightning} />
-                <span className="nav__link__text">Activity</span>
-              </NavLink>
-            </li>
-            <li className="nav__item">
-              <NavLink to="/people" className="nav__link">
-                <img alt="" className="nav__link__icon" src={people} />
-                <span className="nav__link__text">People</span>
-              </NavLink>
-            </li>
-            <li className="nav__item">
-              <NavLink to="/preferences" className="nav__link">
-                <img alt="" className="nav__link__icon" src={pref} />
-                <span className="nav__link__text">Preferences</span>
-              </NavLink>
-            </li>
-            <li className="nav__item">
-              <NavLink to="/security" className="nav__link">
-                <img alt="" className="nav__link__icon" src={shield} />
-                <span className="nav__link__text">Security</span>
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-        <div className="user">
-          <div className="user-img__wrap">{userAvatar}</div>
-          <p className="user-name">{user.name}</p>
-          <p className="user-location">{user.location}</p>
-        </div>
-      </header>
-      <main className="main">
-        <UtilityBar />
+      <Routes>
+        <Route path="/" element={<ErrorBoundary><HomePage setUser={setUser} /></ErrorBoundary>} />
+        {/* <Route path="/*" element={<ErrorPage />} /> */}
+      </Routes>
 
-        <Routes>
-          <Route strict path="/" element={<RepositoriesPage owner={owner} />} />
-          <Route
-             path="/repo/*"
-            element={<IndividualRepoPage owner={owner} />}
-          />
-          <Route strict path="/activity" element={<ActivityPage />} />
-          <Route strict path="/people" element={<PeoplePage />} />
-          <Route strict path="/preferences" element={<PreferencesPage />} />
-          <Route
-            strict
-            path="/security"
-            element={
-              <ErrorBoundary>
+      {!!user && (<section className="view">
+        <header className="header">
+          <div className="logo">
+            <img alt="" className="logo-img" />
+          </div>
+          <nav className="nav">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <NavLink to="/repos" className="nav__link">
+                  <img alt="" className="nav__link__icon" src={stack} />
+                  <span className="nav__link__text">Repositories</span>
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink to="/activity" className="nav__link">
+                  <img alt="" className="nav__link__icon" src={lightning} />
+                  <span className="nav__link__text">Activity</span>
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink to="/people" className="nav__link">
+                  <img alt="" className="nav__link__icon" src={people} />
+                  <span className="nav__link__text">People</span>
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink to="/preferences" className="nav__link">
+                  <img alt="" className="nav__link__icon" src={pref} />
+                  <span className="nav__link__text">Preferences</span>
+                </NavLink>
+              </li>
+              <li className="nav__item">
+                <NavLink to="/security" className="nav__link">
+                  <img alt="" className="nav__link__icon" src={shield} />
+                  <span className="nav__link__text">Security</span>
+                </NavLink>
+              </li>
+            </ul>
+          </nav>
+          <div className="user">
+            <div className="user-img__wrap">{userAvatar}</div>
+            <p className="user-name">{user.name}</p>
+            <p className="user-location">{user.location}</p>
+          </div>
+        </header>
+        <main className="main">
+          <UtilityBar />
+
+          <Routes>
+            <Route strict path="/repos" element={<RepositoriesPage owner={user.login} />} />
+            <Route
+              path="/repos/*"
+              element={<IndividualRepoPage owner={user.login} />}
+            />
+            <Route strict path="/activity" element={<ActivityPage />} />
+            <Route strict path="/people" element={<PeoplePage />} />
+            <Route strict path="/preferences" element={<PreferencesPage />} />
+            <Route
+              strict
+              path="/security"
+              element={
                 <SecurityPage />
-              </ErrorBoundary>
-            }
-          />
-        </Routes>
-      </main>
+              }
+            />
+          </Routes>
+        </main>
+      </section>)}
     </div>
   );
 }
